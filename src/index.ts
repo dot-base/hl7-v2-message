77f020abbb70dda2bc46f38ad59028fedd6e71d6
Andrea Kreichgauer <andrea@kreichgauer.com>
2020-11-09 15:54:32 +0100

    create segment classes, based on from hl7-dictionarydefinitions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import Hl7Dictionary from "@/init/hl7Dictionary";
-import Hl7MessageTemplate from "@/init/hl7DefinitionBuilder";
-import ORU_Message from "@/model/messageTypes/oruMessage";
+import Hl7DefinitionBuilder from "@/init/hl7DefinitionBuilder";
 
 const hl7Dictionary: Hl7Dictionary = Hl7Dictionary.getInstance("2.3");
-const typedMessage = Hl7MessageTemplate.init(hl7Dictionary.definition, new ORU_Message());
+const typedMessage = Hl7DefinitionBuilder.init(hl7Dictionary.definition);
