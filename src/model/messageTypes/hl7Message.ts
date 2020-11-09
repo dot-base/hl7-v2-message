1509573aca0d64e966a90285acd4c4b58a1bdc9c
Andrea Kreichgauer <andrea@kreichgauer.com>
2020-11-09 16:05:17 +0100

    rm types rawMessage and rawSegment

--- src/model/messageTypes/hl7Message.ts
+++ src/model/messageTypes/hl7Message.ts
@@ -4,10 +4,3 @@ import Hl7ISegment from "@/types/hl7ISegment";
 export default abstract class Hl7Message implements Hl7IMessage {
   [key: string]: Hl7ISegment;
 }
-
-/**
- * helper classes only - should be removed
- */
-export class RawHl7Message implements Hl7IMessage {
-  [key: string]: Hl7ISegment;
-}
