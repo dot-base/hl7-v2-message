f33dd180a002f8d191f763a747634058a8aaa9a0
Andrea <andrea@kreichgauer.com>
2020-11-12 23:35:56 +0100

    refactored building types and classFiles

--- src/build.ts
+++ src/build.ts
@@ -1,5 +1,8 @@
-import Hl7DefinitionBuilder from "@/init/hl7Types";
+import Hl7Types from "@/init/hl7Types";
 import { definitions } from "hl7-dictionary";
+import Hl7 from '@/types/hl7';
+import Hl7Model from '@/init/hl7Model';
 
 //TODO: how to handle different versions? init all versions on build here or init only on usage?
-Hl7DefinitionBuilder.init(definitions["2.3"]);
+const initTypes :Hl7 = Hl7Types.init(definitions["2.3"]);
+Hl7Model.createClassFiles(initTypes)
---
+++ src/types/hl7.ts
@@ -0,0 +1,15 @@
+import Hl7IFields from '@/types/hl7IFields';
+import Hl7IMessage from '@/types/hl7IMessage';
+import Hl7ISegment from '@/types/hl7ISegment';
+
+export default class Hl7 {
+  public messages: Hl7IMessage[];
+  public segments: Hl7ISegment[];
+  public fields: {segmentType:string,props:Hl7IFields}[];
+
+constructor( messages: Hl7IMessage[],segments: Hl7ISegment[],fields: {segmentType:string,props:Hl7IFields}[]){
+    this.messages = messages;
+    this.segments=segments;
+    this.fields=fields;
+}
+}
