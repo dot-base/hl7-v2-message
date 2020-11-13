99f4462c8233e6c84c484602dfe09bb55c3d78af
Andrea <andrea@kreichgauer.com>
2020-11-13 14:07:54 +0100

    rm Hl7ISubField from Hl7IField

--- src/types/hl7IField.ts
+++ src/types/hl7IField.ts
@@ -7,20 +7,6 @@ export default interface Hl7IField {
   repeatable?: number;
   value: string;
   subFields?: string[];
-  //value:string|Hl7Subfield
 }
 
-/**
- * TODO: seperate type for Subfield useful?
- * Alternatievely just leave it as array within type Hl7Field
- * Depends on strictness of subfield definiton
- * */
-export interface Hl7ISubfield extends Hl7IField {
-  index: number;
-  identifier: string;
-  description: string;
-  length: number;
-  optional?: boolean;
-  repeatable?: number;
-  value: string;
-}
+
