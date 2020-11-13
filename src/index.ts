e2e1aae916e8b95a85109e66101840419c24fdf2
Andrea <andrea@kreichgauer.com>
2020-11-13 15:07:04 +0100

    rm default exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 //TODO remove default from all classes in /model and export here
 
-/**
-export { Hl7Message } from '@/model/hl7Message';
-export { Hl7Segment } from '@/model/hl7Segment';
-...
- */
+export { Hl7Message } from "@/model/hl7Message";
+export { Hl7Segment } from "@/model/hl7Segment";
+export { Hl7Fields } from "@/model/hl7Fields";
--- src/init/classTemplate.ts
+++ src/init/classTemplate.ts
@@ -4,9 +4,9 @@ export default class ClassTemplate {
     classProps: string,
     description?: string
   ): string {
-    const imports = `import Hl7Fields from '@/model/hl7Fields';\nimport Hl7IField from "@/types/hl7IField";`;
+    const imports = `import { Hl7Fields } from '@/model/hl7Fields';\nimport Hl7IField from "@/types/hl7IField";`;
     const docs = description ? `/**\n* ${description}\n*/` : "";
-    const classDeclaration = `export default class ${type}_Fields extends Hl7Fields`;
+    const classDeclaration = `export class ${type}_Fields extends Hl7Fields`;
     return `${imports}\n${docs}\n${classDeclaration}{\n${classProps}\n}`;
   }
 
@@ -15,9 +15,9 @@ export default class ClassTemplate {
     classProps: string,
     description?: string
   ): string {
-    const imports = `import Hl7Segment from "@/model/hl7Segment";\nimport ${type}_Fields from '@/model/fields/${type.toLowerCase()}Fields';\n`;
+    const imports = `import { Hl7Segment } from "@/model/hl7Segment";\nimport { ${type}_Fields } from '../fields/${type.toLowerCase()}Fields';\n`;
     const docs = description ? `/**\n* ${description}\n*/` : "";
-    const classDeclaration = `export default class ${type}_Segment extends Hl7Segment`;
+    const classDeclaration = `export class ${type}_Segment extends Hl7Segment`;
     return `${imports}\n${docs}\n${classDeclaration}{\n${classProps}\n}`;
   }
 
@@ -27,9 +27,9 @@ export default class ClassTemplate {
     imports?: string,
     description?: string
   ): string {
-    const classImports = `import Hl7Message from '@/model/hl7Message';\nimport Hl7IMessageDefinition from '@/types/hl7IMessageDefinition';\n${imports}`;
+    const classImports = `import { Hl7Message } from '@/model/hl7Message';\nimport Hl7IMessageDefinition from '@/types/hl7IMessageDefinition';\n${imports}`;
     const docs = description ? `/**\n* ${description}\n*/` : "";
-    const classDeclaration = `export default class ${type}_Message extends Hl7Message`;
+    const classDeclaration = `export class ${type}_Message extends Hl7Message`;
     return `${classImports}\n${docs}\n${classDeclaration}{\n${classProps}\n}`;
   }
 }
