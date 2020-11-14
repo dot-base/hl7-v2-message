c2c5e6859c50dbc63b0318f0b3662e84d4146330
Andrea <andrea@kreichgauer.com>
2020-11-14 02:33:31 +0100

    refactored add hl7 model for all hl7verions on build>

--- src/build.ts
+++ src/build.ts
@@ -5,12 +5,22 @@ import Hl7Model from "@/init/hl7Model";
 
 /**
  * Initializes HL7 Types based on stated version and
- * creates class files in /model for messages, segments and fields
+ * creates class files in /model/<version> for HL7 messages, segments and fields
  */
+const versions: version[] = [
+  "2.1",
+  "2.2",
+  "2.3",
+  "2.3.1",
+  "2.4",
+  "2.5",
+  "2.5.1",
+  "2.6",
+  "2.7",
+  "2.7.1",
+];
 
-/**
- * TODO: how to handle different versions?
- * Init and export all versions on build here?
- */
-const initTypes: Hl7 = Hl7Types.init(definitions["2.3"]);
-Hl7Model.createClassFiles(initTypes);
+versions.forEach((version) => {
+  const initTypes: Hl7 = Hl7Types.init(definitions[version]);
+  Hl7Model.createClassFiles(version, initTypes);
+});
