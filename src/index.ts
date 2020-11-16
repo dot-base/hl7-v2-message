4128a56a7caf988dbb58a789410a0109fab4b9a3
Andrea <andrea@kreichgauer.com>
2020-11-16 09:02:38 +0100

    WIP: index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-//TODO remove default from all classes in /model and export here
-
 export { Hl7Message } from "@/model/hl7Message";
 export { Hl7Segment } from "@/model/hl7Segment";
 export { Hl7Fields } from "@/model/hl7Fields";
+export { Hl7Field } from "@/model/hl7Field";
+
+import hl7 from "@/model";
+export { hl7 as default };
---
+++ src/model/index.ts
@@ -0,0 +1,7 @@
+import v2_3 from "./2.3";
+
+declare namespace hl7 {
+  export { v2_3 };
+}
+export { hl7 as default };
+
