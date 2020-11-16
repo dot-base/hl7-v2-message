4e55ff62cdc8646a6271865baae98060fbd3f68c
Andrea <andrea@kreichgauer.com>
2020-11-16 19:41:54 +0100

    fixed identation

--- hl7-dictionary.d.ts
+++ hl7-dictionary.d.ts
@@ -1,61 +1,61 @@
 declare module "hl7-dictionary" {
-    let definitions: Hl7VersionDefiniton;
-    let tables: Hl7Tables;
-  }
+  let definitions: Hl7VersionDefiniton;
+  let tables: Hl7Tables;
+}
   
-  interface Hl7Tables {
-    [key: string]: Hl7Table
-  }
+interface Hl7Tables {
+  [key: string]: Hl7Table
+}
 
-  interface Hl7Table {
-    desc: string;
-    values: { [key: string]: string };
-  }
+interface Hl7Table {
+  desc: string;
+  values: { [key: string]: string };
+}
   
-  type Hl7VersionDefiniton = {
+type Hl7VersionDefiniton = {
   [key :string]: Hl7Defintion;
-  };
+};
   
-  interface Hl7Defintion {
-    fields: { [key: string]: Field };
-    segments: { [key: string]: Segment };
-    messages: { [key: string]: Message };
-  }
+interface Hl7Defintion {
+  fields: { [key: string]: Field };
+  segments: { [key: string]: Segment };
+  messages: { [key: string]: Message };
+}
   
-  interface Message {
-    desc: string;
-    name: string;
-    segments: MessageDefinition;
-  }
+interface Message {
+  desc: string;
+  name: string;
+  segments: MessageDefinition;
+}
   
-  interface MessageDefinition {
-    desc: string;
-    segments: SegmentDefintion[];
-  }
+interface MessageDefinition {
+  desc: string;
+  segments: SegmentDefintion[];
+}
   
-  interface Segment {
-    desc: string;
-    fields: FieldDefintion[];
-  }
+interface Segment {
+  desc: string;
+  fields: FieldDefintion[];
+}
   
-  interface SegmentDefintion {
-    name: string;
-    desc: string;
-    min: number;
-    max: number;
-    children?: SegmentDefintion;
-  }
+interface SegmentDefintion {
+  name: string;
+  desc: string;
+  min: number;
+  max: number;
+  children?: SegmentDefintion;
+}
   
-  interface Field {
-    desc: string;
-    subfields: FieldDefintion[];
-  }
+interface Field {
+  desc: string;
+  subfields: FieldDefintion[];
+}
   
-  interface FieldDefintion {
-    datatype: string;
-    desc: string;
-    opt: number;
-    rep: number;
-    len: number;
-  }
+interface FieldDefintion {
+  datatype: string;
+  desc: string;
+  opt: number;
+  rep: number;
+  len: number;
+}
   
