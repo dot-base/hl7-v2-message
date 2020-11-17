523ffe02f0e6e1e1cde7bd57407f699cf1b29a30
Andrea <andrea@kreichgauer.com>
2020-11-17 17:03:04 +0100

    styling

--- src/template/classTemplate.ts
+++ src/template/classTemplate.ts
@@ -1,32 +1,19 @@
 export default class ClassTemplate {
-  public static fields(
-    type: string,
-    classProps: string,
-    description?: string
-  ): string {
+  public static fields(type: string, classProps: string, description?: string): string {
     const imports = `import { Hl7Fields, Hl7Field } from '../../../model';\n`;
     const docs = description ? `/**\n* ${description}\n*/` : "";
     const classDeclaration = `export class ${type}_Fields extends Hl7Fields`;
     return `${imports}\n${docs}\n${classDeclaration}{\n${classProps}\n}`;
   }
 
-  public static segment(
-    type: string,
-    classProps: string,
-    description?: string
-  ): string {
+  public static segment(type: string, classProps: string, description?: string): string {
     const imports = `import { Hl7Segment } from "../../../model/hl7Segment";\nimport { ${type}_Fields } from '../fields/${type.toLowerCase()}Fields';\n`;
     const docs = description ? `/**\n* ${description}\n*/` : "";
     const classDeclaration = `export class ${type}_Segment extends Hl7Segment`;
     return `${imports}\n${docs}\n${classDeclaration}{\n${classProps}\n}`;
   }
 
-  public static message(
-    type: string,
-    classProps: string,
-    imports?: string,
-    description?: string
-  ): string {
+  public static message(type: string, classProps: string, imports?: string, description?: string): string {
     const classImports = `import { Hl7Message } from '../../../model/hl7Message';\nimport Hl7IMessageDefinition from '../../../types/hl7IMessageDefinition';\n${imports}`;
     const docs = description ? `/**\n* ${description}\n*/` : "";
     const classDeclaration = `export class ${type}_Message extends Hl7Message`;
