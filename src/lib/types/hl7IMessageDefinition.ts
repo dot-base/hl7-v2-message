fcc78431a144ddb6cc58830721358a4bf7aca4d9
Andrea <andrea@kreichgauer.com>
2020-11-19 00:26:10 +0100

    add attribute compounds to type  hl7IMessageDefinition

--- src/lib/types/hl7IMessageDefinition.ts
+++ src/lib/types/hl7IMessageDefinition.ts
@@ -3,5 +3,10 @@ import Hl7ISegment from "./hl7ISegment";
 
 export default interface Hl7IMessageDefinition {
   definition: Hl7ISegmentDefinition;
+  //value is only set, if isSegment true
   value?: Hl7ISegment;
+  //isSegment may only be true, if value is defined
+  isSegment: boolean;
+  //compoundDefinition can be set regardless of isSegment true/false, because of nested compounds
+  compoundDefinition?: Hl7ISegmentDefinition;
 }
