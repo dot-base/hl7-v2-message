72cf79a14cf1691b5094aa3e775e82b16bff33b8
Andrea <andrea@kreichgauer.com>
2020-11-26 17:36:37 +0100

    add type hl7ICompoundDefinition

---
+++ src/lib/types/hl7ICompoundDefinition.ts
@@ -0,0 +1,7 @@
+import Hl7ISegmentDefinition from "./hl7ISegmentDefinition";
+import Hl7ISegment from "./hl7ISegment";
+
+export default interface Hl7ICompoundDefinition {
+  definition: Hl7ISegmentDefinition;
+  compoundDefinition?: Hl7ISegmentDefinition;
+}
