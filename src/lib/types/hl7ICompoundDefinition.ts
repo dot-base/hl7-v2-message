0c1f56d26adc82c3334ec23aa0db157cbfcf4395
Andrea <andrea@kreichgauer.com>
2020-11-26 17:54:35 +0100

    refactored hl7IMessageDefinition

--- src/lib/types/hl7ICompoundDefinition.ts
+++ src/lib/types/hl7ICompoundDefinition.ts
@@ -3,5 +3,5 @@ import Hl7ISegment from "./hl7ISegment";
 
 export default interface Hl7ICompoundDefinition {
   definition: Hl7ISegmentDefinition;
-  compoundDefinition?: Hl7ISegmentDefinition;
+  compoundDefinition?: Hl7ICompoundDefinition;
 }
