fc005f118429275873ec5d2fcbf23688e750a905
Andrea <andrea@kreichgauer.com>
2020-11-26 17:54:35 +0100

    refactored hl7IMessageDefinition

--- src/lib/types/hl7IMessageDefinition.ts
+++ src/lib/types/hl7IMessageDefinition.ts
@@ -1,12 +1,9 @@
 import Hl7ISegmentDefinition from "./hl7ISegmentDefinition";
 import Hl7ISegment from "./hl7ISegment";
+import Hl7ICompoundDefinition from './hl7ICompoundDefinition';
 
-export default interface Hl7IMessageDefinition {
+export default interface Hl7IMessageDefinition<T extends Hl7ISegment> {
   definition: Hl7ISegmentDefinition;
-  //value is only set, if isSegment true
-  value?: Hl7ISegment;
-  //isSegment may only be true, if value is defined
-  isSegment: boolean;
-  //compoundDefinition can be set regardless of isSegment true/false, because of nested compounds
-  compoundDefinition?: Hl7ISegmentDefinition;
+  value: T[];
+  compoundDefinition?: Hl7ICompoundDefinition;
 }
