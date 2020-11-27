2834d85545b7715b1fd16b72d68bdfce3c0c69cc
Christian Friedow <christian@friedow.com>
2020-11-27 15:51:32 +0100

    Update models

--- src/lib/model/Hl7Field.ts
+++ src/lib/model/Hl7Field.ts
@@ -5,8 +5,8 @@ export abstract class Hl7Field implements Hl7IField {
   public abstract identifier: string;
   public abstract description: string;
   public abstract length: number;
-  public abstract optional?: boolean;
-  public abstract repeatable?: number;
+  public abstract isOptional?: boolean;
+  public abstract isRepeatable?: number;
   public abstract value: string;
   public abstract subFields?: string[];
 }
