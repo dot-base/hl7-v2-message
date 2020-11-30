b332fe79421888e2610fbcac12ca200acc582d21
Christian Friedow <christian@friedow.com>
2020-11-30 12:10:34 +0100

    Adjust templates to render library

--- src/static/model/Hl7Fields.ts
+++ src/static/model/Hl7Fields.ts
@@ -1,6 +1,6 @@
 import Hl7IFields from "../types/Hl7IFields";
 import Hl7IField from "../types/Hl7IField";
 
-export abstract class Hl7Fields implements Hl7IFields {
+export default abstract class Hl7Fields implements Hl7IFields {
   [key: string]: Hl7IField;
 }
