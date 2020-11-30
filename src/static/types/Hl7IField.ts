8f95a3ebf6d87598e2ff8ef0f0756b08186e136f
Christian Friedow <christian@friedow.com>
2020-11-30 12:54:58 +0100

    Flag length attribute on fields as optional

--- src/static/types/Hl7IField.ts
+++ src/static/types/Hl7IField.ts
@@ -2,7 +2,7 @@ export default interface Hl7IField {
   index: number;
   identifier: string;
   description: string;
-  length: number;
+  length?: number;
   isOptional?: boolean;
   isRepeatable?: number;
   value: string;
