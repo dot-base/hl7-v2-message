916c1f7ae4a34a4817cd1930a558cf3317b8ff0c
Christian Friedow <christian@friedow.com>
2020-11-30 12:54:58 +0100

    Flag length attribute on fields as optional

--- src/static/model/Hl7Field.ts
+++ src/static/model/Hl7Field.ts
@@ -4,7 +4,7 @@ export default abstract class Hl7Field implements Hl7IField {
   public abstract get index(): number;
   public abstract get identifier(): string;
   public abstract get description(): string;
-  public abstract get length(): number;
+  public abstract get length(): number | undefined;
   public abstract get isOptional(): boolean | undefined;
   public abstract get isRepeatable(): number | undefined;
   public abstract value: string;
