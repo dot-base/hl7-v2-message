93417cc0a22863f3a2331b42529d6d28c113e5ed
Andrea <andrea@kreichgauer.com>
2020-12-01 10:43:07 +0100

    add parsing logic

--- src/static/parser/fieldParser.ts
+++ src/static/parser/fieldParser.ts
@@ -1,24 +1,33 @@
-import Hl7Segment from "../model/Hl7Segment";
+import Hl7IFields from "../types/Hl7IFields";
+import Hl7IMessageSegment from "../types/Hl7IMessageSegment";
+import Hl7ISegment from "../types/Hl7ISegment";
+import { RawSegment } from "./hl7Parser";
 
-interface RawSegment {
-  type: string;
-  fields: string[];
-}
 export default class FieldParser {
-  public static initSegmentFields<T extends Hl7Segment>(segment: T, rawSegment: RawSegment): T {
-    if (rawSegment.fields.length !== Object.entries(segment.fields).length) {
+  public static initSegmentFields<T extends Hl7ISegment>(segment: T, rawSegment: RawSegment): T {
+    if (!FieldParser.validateFieldLength(segment.fields, rawSegment.fields)) {
       throw Error(`Number of fields in raw segment ${rawSegment.type} does not match required number of fields`);
     }
     return FieldParser.setFieldValues(segment, rawSegment);
-    //TODO: add validation by field length here?
   }
-
-  private static setFieldValues<T extends Hl7Segment>(segment: T, rawSegment: RawSegment): T {
+  
+  private static setFieldValues<T extends Hl7ISegment>(segment: T, rawSegment: RawSegment): T {
     rawSegment.fields.forEach((value, index) =>
-      Object.entries(segment.fields).find((field) => {
-        if (field[1].index === index) field[1].value = value;
-      })
+      segment = FieldParser.setFieldValue(segment,value,index)
     );
     return segment;
+  }
+
+  private static setFieldValue<T extends Hl7ISegment>(segment: T, rawValue: string, index:number):T{
+    Object.entries(segment.fields).find((field) => {
+      if (field[1].index === index) field[1].value = rawValue;
+    })
+    return segment;
+  }
+
+  private static validateFieldLength(segmentFields:Hl7IFields, rawSegmentFields:string[]):boolean{
+    if(Object.entries(segmentFields).length && rawSegmentFields.length !== Object.entries(segmentFields).length)
+    return false;
+    return true
   }
 }
---
+++ src/static/parser/messageParser.ts
@@ -0,0 +1,24 @@
+import ADT_A01_Message from "../lib/2.5/message/ADT_A01_Message";
+import ADT_A02_Message from "../lib/2.5/message/ADT_A02_Message";
+import ADT_A03_Message from "../lib/2.5/message/ADT_A03_Message";
+import MDM_T02_Message from "../lib/2.5/message/MDM_T02_Message";
+
+import MSH_Segment from "../lib/2.5/segment/MSH_Segment";
+import Hl7Message from "../model/Hl7Message";
+import { RawSegment } from "./hl7Parser";
+import SegmentParser from "./segmentParser";
+
+export default class MessageParser {
+  public static initMessageSegments(messageType: string, mshSegment: MSH_Segment, rawSegments: RawSegment[]):Hl7Message {
+    if (messageType.includes("ADT_A01"))
+      return SegmentParser.initMessageSegments(new ADT_A01_Message(), mshSegment, rawSegments) as ADT_A01_Message;
+    if (messageType.includes("ADT_A02"))
+      return SegmentParser.initMessageSegments(new ADT_A02_Message(), mshSegment, rawSegments) as ADT_A02_Message;
+    if (messageType.includes("ADT_A03"))
+      return SegmentParser.initMessageSegments(new ADT_A03_Message(), mshSegment, rawSegments) as ADT_A03_Message;
+    if (messageType.includes("MDM_T02"))
+      return SegmentParser.initMessageSegments(new MDM_T02_Message(), mshSegment, rawSegments) as ADT_A03_Message;
+    //TODO: add all types for specific version on build
+    throw Error(`Unknown message type ${messageType}`);
+  }
+}
